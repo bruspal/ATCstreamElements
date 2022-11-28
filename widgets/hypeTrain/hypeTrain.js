c8b7d5d75eaf7831b353d34802bb518e9f3123a7
Bruspal <bruspal@bruspal.net>
2022-11-28 22:22:09 +0400

    Ajout du code de base pour l'anim train de la hype.

---
+++ widgets/hypeTrain/hypeTrain.js
@@ -0,0 +1,281 @@
+class brsSpriteTiles {
+    /* Constructor */
+    constructor(constructorOptions) {
+        constructorOptions = constructorOptions || {};
+        // On controle les options de constructeur
+        this.setConstOptions(constructorOptions, 'imgSrc');
+        this.setConstOptions(constructorOptions, 'selector');
+        this.setConstOptions(constructorOptions, 'rows');
+        this.setConstOptions(constructorOptions, 'cols');
+        // Parametre optionnel
+        // Decallage
+        this.offsetX = constructorOptions.offsetX || 0;
+        this.offsetY = constructorOptions.offsetY || 0;
+        // Crop
+        this.cropTop = constructorOptions.cropTop || 0;
+        this.cropBottom = constructorOptions.cropBottom || 0;
+        this.cropLeft = constructorOptions.cropLeft || 0;
+        this.cropRight = constructorOptions.cropRight || 0;
+
+        // objet recevant la liste des animations créé par createAnimation().
+        this.animationList = {};
+        // Tableau stockant les coordonnées des différente tiles
+        this.tilesList = [];
+        // Variables stockant la ref vers le setInterval()
+        this.frameInterval = null;
+        // tableau stockant la liste des frames a afficher
+        this.framesList = [];
+
+        // Calcul les métrique necessaire
+        this.loaded = false;
+        this.activeFrame = 0;
+        this.img = new Image();
+        this.img.addEventListener("load", ev => this.onImageLoaded(ev));
+        this.img.src = this.imgSrc;
+    };
+
+    /* Methode de mie en place + controle des parametre du constructeur. */
+    setConstOptions(constructorOptions, optionName) {
+        if (optionName in constructorOptions) {
+            this[optionName] = constructorOptions[optionName];
+        } else {
+            throw new Error(`Options '${optionName}' obligatoire`);
+        }
+    }
+
+    /* Calcul les coordonnées des différentes frame disponible pour l'animation */
+    computeTilesList() {
+        let tillNumber = this.rows * this.cols;
+        let x = 0;
+        let y = 0;
+
+        for (y = 0; y < this.rows; y++) {
+            for (x = 0; x < this.cols; x++) {
+                this.tilesList.push([
+                    Math.round((x + this.offsetX) * this.tileWidth),
+                    Math.round((y + this.offsetY) * this.tileHeight)
+                ]);
+            }
+        }
+    }
+
+    /* Calcul le tableau de séquence de frames pour les options fournies */
+    computeOptions(options) {
+        options= {
+            ...{
+                fps: 10,
+                count: 0,
+                reverse: false,
+                pingpong: false
+            },
+            ...options
+        }
+        let framesList = [];
+        // Gestion des sequence
+        if ('from' in options) {
+            // Traitement du 'from' -> 'to'
+            if ( ! ('to' in options)) {
+                throw new Error(`Option d'animation mal formé : Avec l'option 'to', l'option 'from' est obligatoire`);
+            }
+            for (let i = options.from; i <= options.to; i++) {
+                framesList.push(i);
+            }
+        } else if ('frames' in options) {
+            // Traitement de l'option 'frames'
+            framesList = options.frames;
+        } else if ('col' in options) {
+            // to implement
+        } else if ('row' in options) {
+            // to implement
+        } else {
+            framesList = [...this.tilesList.keys()];
+        }
+        // Gestion du reverse
+        if (options.reverse) {
+            framesList.reverse();
+        }
+        // Gestion pingpong
+        if (options.pingpong) {
+            let normal = [...framesList];
+            normal.pop();
+            let reversed = [...framesList.reverse()];
+            framesList = normal.concat(reversed);
+        }
+        // Gestion des répétition
+        if (options.count == 0) {
+            options.loop = true;
+        } else {
+            options.loop = false;
+            options.innerCounter = Math.abs(options.count);
+        }
+        options.framesList = framesList;
+        return options;
+    }
+
+    /* apres préchargement de l'image */
+    onImageLoaded(event) {
+        this.initDom();
+
+        this.tileHeight = event.target.naturalHeight / this.rows;
+        this.tileWidth = event.target.naturalWidth / this.cols;
+
+        // Récupération du selecteur qui contiendra le sprite et pamaétrage de celui-ci
+        this.element.style['height'] = this.tileHeight+"px";
+        this.element.style['width'] = this.tileWidth+"px";
+        this.element.style['background-image'] = `url('${this.imgSrc}')`;
+        this.element.style['background-attachment'] = 'local';
+        this.element.style['background-position-x'] = '0px';
+        this.element.style['background-position-y'] = '0px';
+        this.element.style['background-origin'] = 'unset';
+        this.element.style['background-repeat'] = 'no-repeat';
+        this.element.style['image-rendering'] = 'pixelated';
+
+        // on calcul les coordonnées des différentes tiles
+        this.computeTilesList();
+
+        this.loaded = true;
+    }
+
+    /* Initialise le DOM */
+    initDom() {
+        this.element = document.querySelector(this.selector);
+    }
+
+    /* creer une animation */
+    createAnimation(animationName, options) {
+        this.animationList[animationName] = options;
+    }
+
+    stopAnimation() {
+        clearInterval(this.frameInterval);
+        setTimeout(() => this.activeFrame = 0, 1);
+    }
+
+    startAnimation(animationName, options) {
+        this.activeFrame = 0;
+        runAnimation(animationName, options);
+    }
+
+    /* Lance l'animation */
+    runAnimation(animationName, options) {
+        // controle anim existe
+        if ( ! (animationName in this.animationList)) {
+            throw new Error(`L'animation '${animationName}' n'a pas été définie.`);
+        }
+        // merge des options définie dans l'anim avec les options forcé dans l'appel de runAnimation
+        options = options || {};
+        let localOptions = {...this.animationList[animationName], ...options};
+        // calcul de la séquence de frame
+        localOptions = this.computeOptions(localOptions);
+        this.framesList = localOptions.framesList;
+
+        if (this.frameInterval) clearInterval(this.frameInterval);
+
+        this.frameInterval = setInterval(() => {
+            if ( ! localOptions.loop && this.activeFrame == 0) {
+                localOptions.innerCounter--;
+                if (localOptions.innerCounter == 0) {
+                    clearInterval(this.frameInterval);
+                    // on passe à l'anim suivante si then est défini
+                    if ('then' in localOptions) {
+                        let nextAnim = localOptions.then.animation;
+                        let nextOptions = localOptions.then.options || {};
+                        this.startAnimation(nextAnim, nextOptions)
+                    }
+                }
+            }
+
+            let frameNumber = this.framesList[this.activeFrame];
+            this.element.style['background-position-x'] = (this.tilesList[frameNumber][0] * -1) + "px";
+            this.element.style['background-position-y'] = (this.tilesList[frameNumber][1] * -1) + "px";
+            this.activeFrame = ++this.activeFrame % this.framesList.length;
+        }, 1000 / localOptions.fps);
+    }
+}
+
+// Definition des variables
+let train = null;
+let audioTrain = null;
+
+// Init + gestion anim CSS
+document.addEventListener('DOMContentLoaded', () => {
+    debugger;
+    train = document.getElementById('sprite');
+    /*
+    const runAnimBtn = document.getElementById('runAnim');
+    runAnimBtn.addEventListener('click', () => {
+      train.classList.replace('inactive', 'zoom');
+    })
+    */
+
+    train.addEventListener("animationend", () => {train.classList.replace('zoom', 'inactive');}, false);
+    /*
+      train.addEventListener("animationstart", listener, false);
+      train.addEventListener("animationiteration", listener, false);
+      function listener(event) {
+        switch(event.type) {
+          case "animationstart":
+            console.log(`Started: elapsed time is ${event.elapsedTime}`);
+            break;
+          case "animationend":
+            console.log(`Ended: elapsed time is ${event.elapsedTime}`);
+            train.classList.replace('zoom', 'inactive');
+            break;
+          case "animationiteration":
+            console.log(`New loop started at time ${event.elapsedTime}`);
+            break;
+        }
+      }
+    */
+})
+
+
+// Gestion des events message
+
+window.addEventListener('onEventReceived', function (obj) {
+    const listener = obj.detail.listener;
+    const event = obj.detail.event;
+    log(listener, event);
+    if (listener == 'message') {
+        debugger;
+        let speaker = event.data.nick;
+        let text = event.data.text;
+        if (text == 'touristePORCO Tous à bord... Le train de la HYPE entre en gare !') {
+            triggerTrain();
+        }
+    }
+});
+
+// Init du widget
+window.addEventListener('onWidgetLoad', function (obj) {
+    SEdata = obj.detail.session.data;
+    fields = obj.detail.fieldData;
+
+    // Definition de l'animation
+    const sprite = new brsSpriteTiles({
+        //imgSrc: 'https://cdn.streamelements.com/uploads/1bf22b0b-037f-4f49-9ffe-0d2f9d4a1ccf.png',
+        imgSrc: fields.spritesSheetUrl,
+        selector: '#sprite',
+        rows: 1,
+        cols: 8
+    });
+    sprite.createAnimation('idle', {from: 0, to: 3, fps: fields.animFps});
+    sprite.runAnimation('idle');
+
+    audioTrain = new Audio(fields.audioUrl);
+    audioTrain.addEventListener('loadeddata', () => {
+        console.log('Audio chargé');
+        audioTrain.volume = fields.audioVolume;
+    });
+    const runAnimBtn = document.getElementById('runAnim');
+    runAnimBtn.addEventListener('click', () => {
+        triggerTrain();
+    });
+});
+
+// Lance l'anime
+function triggerTrain() {
+    train.classList.replace('inactive', 'zoom');
+    audioTrain.play();
+}
+
